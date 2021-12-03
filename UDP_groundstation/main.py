df1d724a8d125e37182f30f554f79dd59d5342ea
kahyunnam <kahyunnam9@gmail.com>
2021-12-03 12:13:51 -0500

    clean

--- UDP_groundstation/main.py
+++ UDP_groundstation/main.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python3
 from server.udp_server import udp_server
+import sys
+import socket
 
 if __name__ == "__main__":
+    if (len(sys.args) == 2):
+        udp_addr = "192.168.0." + str(sys.args[1])
+        try:
+            server = udp_server(udp_addr, 3333)
+            server.upload_data("localhost:8000/addData")
+        except socket.error as msg:
+            print ("wrong ip address. Check with ifconfig command on ground station pi")
+    
+    else:
+        print ("call this script with the last 3 digits of the udp server ip address as arg 1")
 
-    server = udp_server("192.168.0.101", 3333)
-    server.upload_data("localhost:8000/addData")
--- UDP_groundstation/server/udp_server.py
+++ UDP_groundstation/server/udp_server.py
@@ -9,29 +9,39 @@ class udp_server:
         """
         create a UDP socket server side
 
-        @param: addr: the IP address of the server
-        @param: port: the port that the server is running in
+        @param addr: the IP address of the server
+        @param port: the port that the server is running in
         """
         self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.serverSock.bind((addr, port))
         print("UDP server up and listening")
 
-    def binary_to_dict(self, the_data):
+
+    def binary_to_dict(self, the_data: bytes):
         """
         decode binary dict data to python string dict
+
+        @param the_data: UTF-encoded data sent from udp client
+        @return decoded string
         """
         dict_str = the_data.decode("UTF-8")
         data_dict = ast.literal_eval(dict_str)
         return data_dict
 
+
     def upload_data(self, api_url: str):
         """
-        listen for data from client and print out data
+        listen for data from client and send to django API
+
+        @param api_url: API endpoint url
         """
         while True:
 
             data, addr = self.serverSock.recvfrom(2048)
             print(f"data received from addr:{addr}")
+
+            # # uncomment for debugging
+            # print(f"data = {self.binary_to_dict(data)}")
 
             headers = {'content-type': 'application/json'}
             response = requests.post(api_url, data=data, headers=headers)
